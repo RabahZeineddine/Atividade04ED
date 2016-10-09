61ddb6b487f2f8d21db0814490d7871c1472c64d
RabahZeineddine <rabah.zeineddine@hotmail.com>
2016-10-09 17:38:19 -0300

    added project

---
+++ src/atividade04/Atividade04.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade04;
+
+import br.mackenzie.fci.estrutura.Util.Util;
+import java.io.File;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author RABAH
+ */
+public class Atividade04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        File file = new File("testeVoos.txt");
+        String[] allCodes = Util.saveCompaniesCodeToArray(file);
+        LocalTime startTime = LocalTime.now();
+        System.out.println("FORMA1:\n Existe "+ Util.getDifferentCompaniesCodes(allCodes).size() +" companhias áreas diferentes");
+        LocalTime finishTime = LocalTime.now();
+        System.out.println("Tempo de execucao para a FORMA1: " + ChronoUnit.MILLIS.between(startTime, finishTime) + " ms");
+        int mid = 0,count = 1;
+        String x = allCodes[0];
+        
+        startTime = LocalTime.now();
+        do {
+            mid = first(allCodes, 0, allCodes.length - 1, x) + 1;
+            if (mid < allCodes.length) {
+                x = allCodes[mid];
+                count++;
+            }
+        } while (mid < allCodes.length);
+        finishTime = LocalTime.now();
+        System.out.println("FORMA2 :\n Existe "+ count +" companhias áreas diferentes");
+        System.out.println("Tempo de execição para a FORMA2: "+ChronoUnit.MILLIS.between(startTime, finishTime)+" ms");
+    }
+
+    public static int first(String arr[], int low, int high, String x) {
+        if (low <= high) {
+            int mid = (low + high) / 2;
+            if (mid + 1 < arr.length) {
+                if (x.equals(arr[mid]) && !x.equals(arr[mid + 1])) {
+                    return mid;
+                } else if (x.equals(arr[mid])) {
+                    return 0 + first(arr, mid + 1, high, x);
+                } else {
+                    return 0 + first(arr, low, mid - 1, x) + first(arr, mid + 1, high, x);
+                }
+            } else if (arr[mid].equals(x)) {
+                return mid;
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/br/mackenzie/fci/estrutura/Util/Util.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.mackenzie.fci.estrutura.Util;
+
+import atividade04.Atividade04;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author RABAH
+ */
+public class Util {
+    
+    public static String[] saveCompaniesCodeToArray(File file) {
+
+        String[] allCodes = new String[quantidadeLinhas(file)];
+        String[] arraySplit;
+        int l = 0;
+        try {
+
+            BufferedReader input = new BufferedReader(new FileReader(file));
+            String line = "";
+            do {
+                line = input.readLine();
+                if(line!=null && !line.equals("")){
+                    arraySplit = line.split(" ");
+                    allCodes[l] = arraySplit[0];
+                    l++;
+                }
+            } while (line != null);
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Atividade04.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Atividade04.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return allCodes;
+    }
+    
+    
+    
+    
+    
+
+    public static int quantidadeLinhas(File file) {
+        LineNumberReader linhaLeitura = null;
+        try {
+            linhaLeitura = new LineNumberReader(new FileReader(file));
+            linhaLeitura.skip(file.length());
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                linhaLeitura.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return linhaLeitura.getLineNumber() + 1;
+    }
+    
+    
+    public static List<String> getDifferentCompaniesCodes(String[] allCodes){
+        String company = allCodes[0];
+        List<String> companiesCodes = new ArrayList<>();
+        companiesCodes.add(company);
+        int l = 0 ;
+        for(int i = 1 ; i < allCodes.length -1 ; i++){
+            company = allCodes[i];
+            if(!company.equalsIgnoreCase(companiesCodes.get(l))){
+                l++;
+                companiesCodes.add(company);
+            }
+        }
+        return companiesCodes;
+    }
+    
+    
+}
