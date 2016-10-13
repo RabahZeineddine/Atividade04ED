d59be23b338c583dab9a77472900d2c2afaa85c2
RabahZeineddine <rabah.zeineddine@hotmail.com>
2016-10-13 11:16:47 -0300

    Modified

--- src/atividade04/Atividade04.java
+++ src/atividade04/Atividade04.java
@@ -24,39 +24,41 @@ public class Atividade04 {
         File file = new File("dataVoos.txt");
         String[] allCodes = Util.saveCompaniesCodeToArray(file);
         LocalTime startTime = LocalTime.now();
-        System.out.println("FORMA1:\n Existe " + Util.getDifferentCompaniesCodes(allCodes).size() + " companhias áreas diferentes");
+        System.out.println("FORMA 1:\n Existe " + Util.getDifferentCompaniesCodes(allCodes).size() + " companhias áreas diferentes");
         LocalTime finishTime = LocalTime.now();
         System.out.println("Tempo de execucao para a FORMA1: " + ChronoUnit.MILLIS.between(startTime, finishTime) + " ms");
-        int mid = 0, count = 1;
-        String x = allCodes[0];
-
         startTime = LocalTime.now();
-        do {
-            mid = first(allCodes, 0, allCodes.length - 1, x) + 1;
-           // System.out.println("mid " + mid + " allCodes.length : " + allCodes.length);
-            if (mid < allCodes.length) {
-                x = allCodes[mid];
-                count++;
-            }
-        } while (mid < allCodes.length);
+        
+        System.out.println("FORMA 2:\n Existe " + (buscaBinaria(allCodes, 0, allCodes.length - 1)+1) + " companhias áreas diferentes");
         finishTime = LocalTime.now();
-        System.out.println("FORMA2 :\n Existe " + count + " companhias áreas diferentes");
         System.out.println("Tempo de execição para a FORMA2: " + ChronoUnit.MILLIS.between(startTime, finishTime) + " ms");
     }
+    
+//    public static int first(String arr[], int low, int high, String x) {
+//        if (low <= high) {
+//            int mid = (low + high) / 2;
+//            if (mid + 1 < arr.length) {
+//                if (x.equals(arr[mid]) && !x.equals(arr[mid + 1])) {
+//                    return mid;
+//                } else if (x.equals(arr[mid])) {
+//                    return first(arr, mid + 1, high, x);
+//                } else {
+//                    return first(arr, low, mid - 1, x) + first(arr, mid + 1, high, x);
+//                }
+//            } else if (x.equals(arr[mid])) {
+//                return mid;
+//            }
+//        }
+//        return 0;
+//    }
 
-    public static int first(String arr[], int low, int high, String x) {
-        if (low <= high) {
+    public static int buscaBinaria(String[] arr, int low, int high) {
             int mid = (low + high) / 2;
-            if (mid + 1< arr.length ) {
-                if (x.equals(arr[mid]) && !x.equals(arr[mid + 1])) {
-                    return mid;
-                } else if (x.equals(arr[mid])) {
-                    return 0 + first(arr, mid + 1, high, x);
-                } else {
-                    return 0 + first(arr, low, mid - 1, x) + first(arr, mid + 1, high, x);
-                }
-            }else if( x.equals(arr[mid])) return mid;
-        }
+            if (!arr[low].equals(arr[high])) {
+                return buscaBinaria(arr, low, mid)+buscaBinaria(arr,mid+1,high);
+            } else if (!arr[high].equals(arr[(high<arr.length-1)?high + 1:high])) {
+                return 1;
+            }
         return 0;
-    }
+    }   
 }
